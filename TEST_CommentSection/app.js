fd840a60c1e9edaf5b990fe2d9b6ee71a158ef3c
NimaYM <nimayqmrmn@gmail.com>
2020-06-26 15:36:26 +0430

    Build NodeJS Server

---
+++ TEST_CommentSection/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const commentRouter = require("./commentRoutes");
+
+const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.static(`${__dirname}/public`));
+
+app.use(commentRouter);
+
+module.exports = app;
