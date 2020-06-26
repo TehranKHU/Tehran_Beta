3b05a2965248dc958613d96fa4784577e0887cc4
NimaYM <nimayqmrmn@gmail.com>
2020-06-26 19:14:39 +0430

    Connecting to Database

--- TEST_CommentSection/app.js
+++ TEST_CommentSection/app.js
@@ -9,7 +9,7 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(`${__dirname}/styles`));
 
 app.use(commentRouter);
 
--- TEST_CommentSection/commentController.js
+++ TEST_CommentSection/commentController.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const con = require("./DBConnection");
 
 exports.getAllComments = async (req, res) => {
   try {
@@ -7,6 +8,20 @@ exports.getAllComments = async (req, res) => {
       .status(200)
       .type("html")
       .sendFile(path.join(`${__dirname}/index.html`));
+
+    //EXECUTE QUERY
+    var conn = con.getConnection();
+    conn.query("SELECT * FROM comments.comments", function (
+      error,
+      results,
+      fields
+    ) {
+      if (error) throw error;
+      results.forEach((comment) => {
+        console.log(comment.userName);
+      });
+    });
+    conn.end();
   } catch (err) {
     res.status(404).json({
       status: "fail",
